0db135809bca614bf4e9966c17e783543923c68b
Junfei Zhang <zhangjunfly@gmail.com>
2017-01-08 00:20:28 -0800

    all tests passed

--- src/main/java/com/tedneward/example/Person.java
+++ src/main/java/com/tedneward/example/Person.java
@@ -3,30 +3,83 @@ package com.tedneward.example;
 import java.beans.*;
 import java.util.*;
 
-public class Person {
+public class Person implements Comparable<Person>{
   private int age;
   private String name;
   private double salary;
   private String ssn;
   private boolean propertyChangeFired = false;
-  
+  private int count = 0;
+
   public Person() {
     this("", 0, 0.0d);
   }
-  
+
   public Person(String n, int a, double s) {
     name = n;
     age = a;
     salary = s;
+    count++;
+  }
+
+  public int count(){
+    return count;
+  }
+
+  public int compareTo(Person other){
+    return (int) (other.getSalary() - this.getSalary());
+  }
+
+  @Override
+  public boolean equals(Object other){
+    if(other instanceof Person){
+      Person temp = (Person) other;
+      return this.name.equals(temp.name) && this.age == temp.age;
+    }
+    return false;
+  }
+
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    if(age < 0)
+      throw new IllegalArgumentException();
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    if(name == null)
+      throw new IllegalArgumentException();
+    this.name = name;
+  }
+
+  public double getSalary() {
+    return salary;
+  }
+
+  public void setSalary(double salary) {
+    this.salary = salary;
+  }
+
+  public String getSsn() {
+    return ssn;
   }
 
   public void setSSN(String value) {
     String old = ssn;
     ssn = value;
-    
+
     this.pcs.firePropertyChange("ssn", old, value);
     propertyChangeFired = true;
   }
+
   public boolean getPropertyChangeFired() {
     return propertyChangeFired;
   }
@@ -34,27 +87,46 @@ public class Person {
   public double calculateBonus() {
     return salary * 1.10;
   }
-  
+
   public String becomeJudge() {
     return "The Honorable " + name;
   }
-  
+
   public int timeWarp() {
     return age + 10;
   }
-  
-  public String tostring() {
-    return "{{FIXME}}";
+
+  @Override
+  public String toString() {
+    return "[Person name:" + name + " age:" + age + " salary:" + salary + "]";
   }
+
 
   // PropertyChangeListener support; you shouldn't need to change any of
   // these two methods or the field
   //
   private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
+
   public void addPropertyChangeListener(PropertyChangeListener listener) {
-      this.pcs.addPropertyChangeListener(listener);
+    this.pcs.addPropertyChangeListener(listener);
   }
+
   public void removePropertyChangeListener(PropertyChangeListener listener) {
-      this.pcs.removePropertyChangeListener(listener);
+    this.pcs.removePropertyChangeListener(listener);
+  }
+
+  public static ArrayList<Person> getNewardFamily(){
+    ArrayList<Person> ans = new ArrayList<>();
+    ans.add(new Person("Ted", 41, 250000));
+    ans.add(new Person("Charlotte", 43, 150000));
+    ans.add(new Person("Michael", 22, 10000));
+    ans.add(new Person("Matthew", 15, 0));
+    return ans;
+  }
+
+  public static class AgeComparator implements Comparator<Person> {
+    public int compare(Person p1, Person p2){
+      return p1.age - p2.age;
+    }
   }
 }
